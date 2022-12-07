d4de97a0f214f9bb4bcfcb0e9cd3e7d4abda3a72
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-12-07 12:52:37 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,62 @@
+export const item = {
+    "id": "864c5f1a-736a-473f-bcb7-15cfb9ebc1c3",
+    "trending_recordings": [
+        {
+            "id": "0ef3ab6f-de20-49a0-8a77-9d4d514f1ed9",
+            "recording_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/155.png"
+        },
+        {
+            "id": "1b38e4c3-8dc8-44a6-a41b-ab0081f315a0",
+            "recording_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/344.png"
+        },
+        {
+            "id": "0cdaefff-cdaf-4ab0-ba81-14bb9026d0b0",
+            "recording_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/327.png"
+        },
+        {
+            "id": "d4eb464b-a38a-4082-883f-f5e4d1799ab9",
+            "recording_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/218.png"
+        },
+        {
+            "id": "239b567f-dc66-40d2-b17d-254412572f86",
+            "recording_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/160.png"
+        }
+    ],
+    "song_lists": [
+        {
+            "id": "0614ca29-dc3b-4207-bd35-cfad82096761",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/319.png",
+            "song_name": "physical",
+            "movie_name": "traditional",
+            "user_name": "dog"
+        },
+        {
+            "id": "1cc061e2-75f1-4ca2-bd01-4a46a5ca78f9",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/144.png",
+            "song_name": "report",
+            "movie_name": "understand",
+            "user_name": "can"
+        },
+        {
+            "id": "28acf8b9-f12d-45cd-83d2-f68580c8522d",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/76.png",
+            "song_name": "agency",
+            "movie_name": "same",
+            "user_name": "also"
+        },
+        {
+            "id": "ea00938a-3949-4d9b-bf81-2157c115a637",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/21.png",
+            "song_name": "color",
+            "movie_name": "game",
+            "user_name": "within"
+        },
+        {
+            "id": "0f328cfc-632f-4e1e-b06c-fb08ad621d06",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/215.png",
+            "song_name": "order",
+            "movie_name": "series",
+            "user_name": "if"
+        }
+    ]
+}
